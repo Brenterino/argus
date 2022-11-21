984325ef9cba1720c325a28243e49f821257f3bb
Zygon <me@zygon.dev>
2022-11-21 17:27:58 -0600

    Initial project setup

---
+++ argus-commons/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+	`java-library`
+	id("io.freefair.lombok")
+	id("org.javamodularity.moduleplugin")
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
+}
+
+val quarkusVersion: String by project
+
+dependencies {
+	// Quarkus BoM
+	implementation(enforcedPlatform("io.quarkus:quarkus-bom:${quarkusVersion}"))
+
+	// Slf4j
+	implementation("org.slf4j:slf4j-api")
+
+	// Test Dependencies
+	testImplementation("org.junit.jupiter:junit-jupiter-api")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks {
+	test {
+		useJUnitPlatform()
+	}
+}
+
+tasks.withType<JavaCompile> {
+	options.encoding = "UTF-8"
+}
---
+++ argus-location-services/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    id("io.freefair.lombok")
+    id("org.javamodularity.moduleplugin")
+    id("io.quarkus")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+val quarkusVersion: String by project
+
+dependencies {
+    // Quarkus BoM
+    implementation(enforcedPlatform("io.quarkus:quarkus-bom:${quarkusVersion}"))
+
+    // Slf4j
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.jboss.slf4j:slf4j-jboss-logmanager")
+
+    // Test Dependencies
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
