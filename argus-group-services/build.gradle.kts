d4cd49494304455afcf4ba8bb8d8c7fa9e9b2c0a
Zygon <me@zygon.dev>
2023-06-25 19:13:58 -0500

    Reverting back to Java 17, updating nginx, making Cloth Config required mod

--- argus-group-services/build.gradle.kts
+++ argus-group-services/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_20
-    targetCompatibility = JavaVersion.VERSION_20
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 val quarkusVersion: String by project
